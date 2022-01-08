890a4a97d544a14c9427c4e60e6a5c660fef4364
PS Prasanth <psprasanth676@gmail.com>
2022-01-08 19:21:01 +0530

    Using divisions Instead of tables
    > Using divisions instead of tables
    > Added the title bar icon

--- script.js
+++ script.js
@@ -19,28 +19,28 @@ app.controller("gridsController", function($scope){
 	  	// 	hr.style.top = i + "px";
 	  	// 	grids.appendChild(hr);
 	  	// }
-	  	var table = document.getElementById("gridsTable")
-	  	if (table == null) {
-		  	table = document.createElement("table")
-		  	table.setAttribute("id", "gridsTable")
-		} else {
-		  	while (table.firstChild) {
-    			table.firstChild.remove()
-			}
+	  	while (grids.firstChild) {
+			grids.firstChild.remove()
 		}
-	  	for (var i =0; i< image.offsetHeight; i+= gridSize) {
-	  		var tr = document.createElement("tr")
+	  	for (var i =0; i < image.offsetHeight; i+= gridSize) {
 	  		for (var j = 0; j < image.offsetWidth ; j += gridSize) {
-		  		var td = document.createElement("td")
 		  		var div = document.createElement("div")
-		  		div.style.width = gridSize + "px";
-		  		div.style.height = gridSize + "px";
-		  		td.appendChild(div)
-		  		tr.appendChild(td);
+		  		if (i + gridSize > image.offsetHeight) {
+		  			div.style.height = image.offsetHeight - i -1 + "px"
+		  		} else {
+		  			div.style.height = gridSize + "px";
+		  		}
+		  		if (j + gridSize > image.offsetWidth) {
+		  			div.style.width = gridSize  -j  -1 + "px";
+		  		} else {
+		  			div.style.width = gridSize + "px";
+		  		}
+		  		div.style.top = i + "px";
+		  		div.style.left = j + "px";
+		  		div.setAttribute("class", "div-grids")
+		  		grids.appendChild(div)
 		  	}
-	  		table.appendChild(tr);
 	  	}
-	  	grids.appendChild(table);
 	}
 
 
